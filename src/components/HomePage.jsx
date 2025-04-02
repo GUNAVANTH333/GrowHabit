3e78a75420c4358c4404089f09f7ad53d7c16503
GUNAVANTH333 <guna2006.3@gmail.com>
2025-04-02 23:13:42 +0530

    added css

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,9 @@
+import "./HomePage.css"
 function NavBar() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Welcome to GrowHabit</h1>
+      <h2>GrowHabit helps you keep track of your habits and motivate you more to follow your habits.</h2>
     </>
   );
 }
