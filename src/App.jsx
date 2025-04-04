bb012eca50ee4acb6f9b8f1023dddaa702d083d8
GUNAVANTH333 <guna2006.3@gmail.com>
2025-04-04 23:46:21 +0530

    added habits counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import NavBar from "./components/HomePage"
+import Tracker from "./components/Tracker/Tracker.jsx";
 function App() {
     return (
         <>
-        <NavBar/>
-        {/* <h1>Welcome to GrowHabit</h1>
-        <h2>GrowHabit helps you keep track of your habits and motivate you more to follow your habits.</h2> */}
+        <Tracker/>
         </>
     )
 }
--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
-
+import "./Tracker.css"
 function Tracker() {
     const [habits,setNewHabit] = useState(0)
+    const updateHabits = () => {
+        setNewHabit(habits+1)
+    }
     return (
         <>
         <div className="add_habit">
-            <button onClick={setNewHabit(habits+1)}></button>
+            <h1>Create A new Habit</h1>
+            <button onClick={updateHabits}>Add New Habit</button>
         </div>
         </>
     );
 }
+export default Tracker;
