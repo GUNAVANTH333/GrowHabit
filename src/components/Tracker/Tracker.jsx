c12562660940e5180f69ff7a94bbd0040b50f51e
GUNAVANTH333 <guna2006.3@gmail.com>
2025-04-06 22:40:04 +0530

    added css

--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -4,6 +4,10 @@ function Tracker() {
     const [habits,setNewHabit] = useState([])
     const [input,setInput] = useState('')
     const updateHabits = () => {
+        if (input.trim() === '') {
+            alert('Please enter a Habit before adding it.');
+            return;
+        }
         setNewHabit([...habits,input])
         console.log(habits)
         setInput('')
@@ -12,6 +16,7 @@ function Tracker() {
         <>
         <h1>Create A new Habit</h1>
         <input value= {input} placeholder="Type new Habit..." onChange={(event) => setInput(event.target.value)}></input>
+
         <button onClick={updateHabits}>Add Habit</button>
         <ul>
         {habits.map((i) => {
