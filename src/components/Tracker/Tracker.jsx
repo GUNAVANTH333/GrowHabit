64c9af40548f33843173efdfd01263a1a851a6df
GUNAVANTH333 <guna2006.3@gmail.com>
2025-04-06 22:23:57 +0530

    now you can add habits dad q

--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 import "./Tracker.css"
 function Tracker() {
-    const [habits,setNewHabit] = useState({})
+    const [habits,setNewHabit] = useState([])
+    const [input,setInput] = useState('')
     const updateHabits = () => {
-        setNewHabit()
+        setNewHabit([...habits,input])
+        console.log(habits)
+        setInput('')
     }
     return (
         <>
         <h1>Create A new Habit</h1>
-        <div id="add_habit">
+        {/* <div id="add_habit">
             <input placeholder="Enter new Habit..."/>
             <button onClick={updateHabits}>Add</button>
-        </div>
+        </div> */}
+        <input value= {input} placeholder="Type new Habit..." onChange={(event) => setInput(event.target.value)}></input>
+        <button onClick={updateHabits}>Add Habit</button>
+        <ul>
+        {habits.map((i) => {
+            return <li className="habit_element">{i}</li>
+        })}
+        </ul>
         </>
     );
 }
